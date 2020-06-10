2add0c9a93988ad17da9ad3821d44592ff51abe0
྅༻ Ǭɀħ ༄༆ཉ <ozh@ozh.org>
2020-06-10 19:40:42 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Example Plugin
 Plugin URI: https://github.com/ozh/example-plugin
 Description: This plugin does nothing but is installable with Composer
-Version: 1.0
+Version: 2.0
 Author: Ozh
 Author URI: https://ozh.org/
 */
